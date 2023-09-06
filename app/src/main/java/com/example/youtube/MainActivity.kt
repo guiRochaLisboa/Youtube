a7a0b8004c9f7eeabe6d597f69eacf50954d0cfd
Guilherme Rocha <guilhrme161201@gmail.com>
2023-09-06 00:45:23 -0300

    Fazendo requisição pelo OkHttpClient, adicionando diversos ic que serão usados no app, adicionando a nossa toolbar na nossa activity_main.xml pelo material utilizando scroll responsivo conforme o usuário scrola a tela, adicionado as dependencias do kotlin coroutine, picasso para baixar nossas imagens, e gson para converter nossa resposta da api.

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -3,18 +3,76 @@ package com.example.youtube
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.Menu
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var videoAdapter : VideoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rv_main.layoutManager = LinearLayoutManager(this)
-        rv_main.adapter = VideoAdapter()
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
 
+        val videos = mutableListOf<Video>()
+        videoAdapter = VideoAdapter(videos){video ->
+            println(video)
+        }
+
+        rv_main.layoutManager = LinearLayoutManager(this)
+        rv_main.adapter = videoAdapter
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = async { getVideo() }
+            val listVideo = res.await()
+            withContext(Dispatchers.Main){
+                listVideo?.let {
+                    videos.clear()
+                    videos.addAll(listVideo.data)
+                    videoAdapter.notifyDataSetChanged()
+                    progress_recycler.visibility = View.GONE
+                }
+            }
+
+        }
+    }
+
+    //Função chamada para criação do nosso menu na toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    //Na chamada na nossa api
+    private fun getVideo() : ListVideo? {
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .get()
+            .url("https://tiagoaguiar.co/api/youtube-videos")
+            .build()
+
+        return try {
+        val response = client.newCall(request).execute()
+
+            if(response.isSuccessful){
+                GsonBuilder().create()
+                    .fromJson(response.body()?.string(), ListVideo::class.java)
+            }else{
+                null
+            }
+        }catch (e: Exception){
+            null
+        }
     }
 }
