9aa8bcd565ae929de7bf9b4280a8300aaf1ad6ff
Guilherme Rocha <guilhrme161201@gmail.com>
2023-09-13 15:23:04 -0300

    Implementando vídeos recomendados ao selecionar um vídeo na main_activity, conhecendo sobre DSL no kotlin criando nossa classes VideoBuilder e PublisherBuilder no Videos.kt para implementar dados temporarios no app.

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -8,9 +8,11 @@ import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.GsonBuilder
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.video_detail.*
 import kotlinx.android.synthetic.main.video_detail.view.*
+import kotlinx.android.synthetic.main.video_detail_content.*
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -93,6 +95,16 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        val detailAdapter = VideoDetailAdapter(videos())
+        rv_similar.layoutManager = LinearLayoutManager(this)
+        rv_similar.adapter = detailAdapter
+
+        content_channel.text = video.publisher.name
+        content_title.text = video.title
+        Picasso.get().load(video.publisher.pictureProfileUrl).into(img_channel)
+
+        detailAdapter.notifyDataSetChanged()
     }
 
     //Função chamada para criação do nosso menu na toolbar
--- app/src/main/java/com/example/youtube/Videos.kt
+++ app/src/main/java/com/example/youtube/Videos.kt
@@ -26,5 +26,86 @@ data class ListVideo(
     val data: List<Video>
 )
 
+class VideoBuilder {
+    var id: String = ""
+    var thumbnailUrl: String= ""
+    var title: String= ""
+    var publishedAt: Date = Date()
+    var viewsCount: Long = 0
+    var viewsCountLabel: String= ""
+    var duration: Int = 0
+    var videoUrl: String= ""
+    var publisher: Publisher = PublisherBuilder().build()
+
+    fun build() : Video = Video(
+        id, thumbnailUrl, title, publishedAt, viewsCount, viewsCountLabel, duration, videoUrl, publisher
+    )
+}
+
+class PublisherBuilder {
+    var id: String= ""
+    var name: String = ""
+    var pictureProfileUrl: String = ""
+
+    fun build() : Publisher =
+        Publisher(id,name,pictureProfileUrl)
+}
+
+//DSL do kotlin
+fun video(block: VideoBuilder.() -> Unit): Video =
+    VideoBuilder().apply(block).build()
+
+fun publisher(block: PublisherBuilder.() -> Unit): Publisher =
+    PublisherBuilder().apply(block).build()
+
+fun videos() : List<Video>{
+    return arrayListOf(
+        video {
+            id = "UVpKBHO2fMg"
+            thumbnailUrl = "https://img.youtube.com/vi/UVpKBHO2fMg/maxresdefault.jpg"
+            title = "Entrevista com Marlon Wayans | The Noite (14/08/19)"
+            publishedAt = "2019-08-15".toDate()
+            viewsCount = 742_497
+            duration = 1886
+            publisher {
+                id = "sbtthenoite"
+                name = "The Noite com Danilo Gentili"
+                pictureProfileUrl = "https://yt3.ggpht.com/a/AGF-l7_3BYlSlp94WOjGe1UECUCdb73qRJVFH_t9Tw=s48-c-k-c0xffffffff-no-rj-mo"
+            }
+        },
+    video{
+        id = "PlYUZU0H5go"
+        thumbnailUrl = "https://img.youtube.com/vi/cuau8E6t2QU/maxresdefault.jpg"
+        title = "Relembrando Steve Jobs"
+        publishedAt = "2019-08-15".toDate()
+        viewsCount = 1703
+        duration = 194
+        publisher {
+            id = "UCrWWMZ6GVOM5zqYAUI44XXg"
+            name = "Tiago Aguiar"
+            pictureProfileUrl = "https://yt3.ggpht.com/ytc/AKedOLT2VtZ3n30tTpDyjAoZGl44EfHhajN1Zy5LYm3iiA=s88-c-k-c0x00ffffff-no-rj"
+        }
+    },
+        video{
+            id = "-y1HhAlAOTs"
+            thumbnailUrl = "https://img.youtube.com/vi/-y1HhAlAOTs/maxresdefault.jpg"
+            title = "MARRIAGE STORY Official Trailer (2019) Scarlett Johansson, Adam Driver Netflix Movie HD"
+            publishedAt = "2019-08-21".toDate()
+            viewsCount = 1136717
+            duration = 160
+            publisher {
+                id = "movietrailers"
+                name = "ONE Media"
+                pictureProfileUrl = "https://yt3.ggpht.com/a/AGF-l7_0zGH3p2Yu7hMMZVAjwS7H8Ct6qu_vNmGj=s48-c-k-c0xffffffff-no-rj-mo"
+            }
+        }
+
+
+    )
+}
+
 fun Date.formatted() : String =
     SimpleDateFormat("d/MM/yyyy", Locale("pt","BR")).format(this)
+
+fun String.toDate() : Date =
+    SimpleDateFormat("yyyy-mm-dd", Locale("pt","BR")).parse(this)
